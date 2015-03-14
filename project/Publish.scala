86f35cd876e2b9f3c3ab624da702c0a01688e1c5
Tom Switzer <thomas@stripe.com>
2015-03-13 20:00:42 -0700

    Add PGP SBT plugin to sign artifacts.

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,5 +1,6 @@
 import sbt._
 import sbt.Keys._
+import com.typesafe.sbt.pgp.PgpKeys._
 
 object Publish {
   val settings = Seq(
@@ -12,8 +13,14 @@ object Publish {
       else
         Some("sonatype-nexus-staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2/")
     },
+    useGpg := true,
     publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
     pomExtra := (
+      <scm>
+        <url>git@github.com:stripe/brushfire.git</url>
+        <connection>scm:git:git@github.com:stripe/brushfire.git</connection>
+      </scm>
       <developers>
         <developer>
           <name>Avi Bryant</name>
