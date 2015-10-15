46e8806b8f10abca148f592d7cdab07740952916
Tom Switzer <thomas@stripe.com>
2015-10-15 14:51:03 -0400

    Use sbt-release.

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,29 +1,39 @@
 import sbt._
 import sbt.Keys._
-import com.typesafe.sbt.pgp.PgpKeys._
+
 import aether.AetherKeys._
+import com.typesafe.sbt.pgp.PgpKeys
+import sbtrelease.ReleasePlugin.autoImport._
+import sbtrelease.ReleaseStateTransformations._
 
 object Publish {
+  val nexus = "https://oss.sonatype.org/"
+  def stripeSnapshotRepo = sys.props.get("stripe.snapshots.url").map("stripe-nexus" at _)
+  def sonatypeSnapshotRepo = Some("Snapshots" at nexus + "content/repositories/snapshots")
+  def sonatypeStagingRepo = Some("Releases" at nexus + "service/local/staging/deploy/maven2")
+
   val settings = Seq(
-    homepage := Some(url("http://github.com/stripe/brushfire")),
-    licenses += ("MIT License", url("http://www.opensource.org/licenses/mit-license.php")),
+    releaseCrossBuild := true,
+    releasePublishArtifactsAction := PgpKeys.publishSigned.value,
     publishMavenStyle := true,
     publish := {
-      if (isSnapshot.value) {
+      if (isSnapshot.value && stripeSnapshotRepo.isDefined) {
         aetherDeploy.value
       } else {
         publish.value
       }
     },
     publishTo := {
-      if (isSnapshot.value)
-        sys.props.get("stripe.snapshots.url").map("stripe-nexus" at _)
-      else
-        Some("sonatype-nexus-staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+      if (isSnapshot.value) {
+        stripeSnapshotRepo orElse sonatypeSnapshotRepo
+      } else {
+        sonatypeStagingRepo
+      }
     },
-    useGpg := true,
     publishArtifact in Test := false,
-    pomIncludeRepository := { _ => false },
+    pomIncludeRepository := Function.const(false),
+    homepage := Some(url("http://github.com/stripe/brushfire")),
+    licenses += ("MIT License", url("http://www.opensource.org/licenses/mit-license.php")),
     pomExtra := (
       <scm>
         <url>git@github.com:stripe/brushfire.git</url>
@@ -36,6 +46,19 @@ object Publish {
           <organization>Stripe</organization>
           <organizationUrl>https://stripe.com</organizationUrl>
         </developer>
-      </developers>)
+      </developers>),
+    releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runClean,
+      ReleaseStep(action = Command.process("package", _)),
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      ReleaseStep(action = Command.process("publishSigned", _)),
+      setNextVersion,
+      commitNextVersion,
+      ReleaseStep(action = Command.process("sonatypeReleaseAll", _)),
+      pushChanges)
   )
 }
